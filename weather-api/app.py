1517c5b1627b59482491413f008b368db7388f94
jurabek-abd <jurabek.pro.abd@gmail.com>
2026-01-14 16:11:57 +0500

    api rate limiting added to weather api using flask-limiter

--- weather-api/app.py
+++ weather-api/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from routes.weather_routes import weather_bp
 from utils.error_handler import register_error_handlers
 
 app = Flask(__name__)
 register_error_handlers(app)
 app.register_blueprint(weather_bp)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["20 per day", "5 per hour"],
+    storage_uri="redis://localhost:6379",
+)
